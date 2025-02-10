e5ea9cb29b81134b57d720c90ddb975602e7f375
luizrodd_ <luizbr080604@gmail.com>
2025-02-10 10:48:50 -0300

    angular template start with tailwind/material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatSlideToggleModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
